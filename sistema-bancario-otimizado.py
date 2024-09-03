ed535a06ae6198b7033798abf24f64e08b4dc28a
Jemima Moura <jemimahelen@hotmail.com>
2024-09-03 18:07:53 -0300

    implementando funcoes

--- sistema-bancario-otimizado.py
+++ sistema-bancario-otimizado.py
@@ -1,20 +1,93 @@
-# regras de negocio
+#regras de negocio
 saldo = 0
 limite = 500
 extrato = ''
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-
+#menu
 def menu():
     print('''
           Menu
           1 - Depósito
           2 - Saque
           3 - Extrato
+          4 - Cadastrar Cliente
+          5 - Criar Conta Corrente
           0 - Sair
           ''')
     
+
+#funcao depositar
+def depositar(saldo, valor, extrato):
+    print('\nDepósito\n')
+    valor = float(input('Qual quantia deseja depositar: '))
+    if valor > 0:
+        saldo += valor
+        extrato += f'Depósito: R$ {valor:.2f}\n'
+        print('Operação realizada com sucesso.')
+    else:
+        print('Não foi possível concluir a operação. Valor informado é inválido.')
+
+    '''
+    sugestao de retorno
+    return saldo, extrato
+    '''
+
+
+#funcao sacar
+def sacar(*, saldo, valor, extrato, limite, numero_saques, LIMITE_SAQUES):
+    print('\nSaque\n')
+    valor = float(input('Qual quantia deseja sacar: '))
+    if valor > saldo:
+        print('Não foi possível concluir a operação. Saldo insuficiente.')
+    elif valor> limite:
+        print('Não foi possível concluir a operação. O valor excede o limite de saque.')
+    elif numero_saques >= LIMITE_SAQUES:
+        print('Não foi possível concluir a operação. Foi atingido o limite de saques diário.')
+    elif valor > 0:
+        saldo -= valor
+        extrato += f'Saque: R$ {valor:.2f}\n'
+        numero_saques += 1
+        print('Operação realizada com sucesso.')
+    else: 
+        print('Não foi possível concluir a operação. Valor informado é inválido.')
+
+    '''
+    sugestao de retorno
+    return saldo, extrato
+    '''
+
+
+#funcao exibir_extrato
+def exibir_extrato(saldo, *, extrato):
+    print('\nExtrato\n')
+    if len(extrato) > 0:
+        print(extrato)
+        print(f'Saldo: R$ {saldo:.2f}')
+    else:
+        print('Não foram realizadas movimentações.')
+
+
+#funcao cadastrar_cliente
+def cadastrar_cliente():
+    clientes = {
+        'nome' : {},
+        'data_nascimento' : {},
+        'cpf' : {},
+        'endereço': {'logradouro', 'nro', 'bairro', 'cidade_estado'},
+    }
+    print('funcao em construcao')
+
+
+#funcao criar_conta
+def criar_conta_corrente(agencia='0001', numero_conta, cliente):
+    contas = []
+
+
+    print('funcao em construcao')
+
+
 
 while True:
 
@@ -22,42 +95,23 @@ while True:
     opcao = input('Escolha uma opção: ')
     
     if opcao == '1':
-        print('\nDepósito\n')
-        valor_deposito = float(input('Qual quantia deseja depositar: '))
-        if valor_deposito > 0:
-            saldo += valor_deposito
-            extrato += f'Depósito: R$ {valor_deposito:.2f}\n'
-            print('Operação realizada com sucesso.')
-        else:
-            print('Não foi possível concluir a operação. Valor informado é inválido.')
+        depositar()
 
     elif opcao == '2':
-        print('\nSaque\n')
-        valor_sacado = float(input('Qual quantia deseja sacar: '))
-        if valor_sacado > saldo:
-            print('Não foi possível concluir a operação. Saldo insuficiente.')
-        elif valor_sacado > limite:
-            print('Não foi possível concluir a operação. O valor excede o limite de saque.')
-        elif numero_saques >= LIMITE_SAQUES:
-            print('Não foi possível concluir a operação. Foi atingido o limite de saques diário.')
-        elif valor_sacado > 0:
-            saldo -= valor_sacado
-            extrato += f'Saque: R$ {valor_sacado:.2f}\n'
-            numero_saques += 1
-            print('Operação realizada com sucesso.')
-        else: 
-            print('Não foi possível concluir a operação. Valor informado é inválido.')
+        sacar()
 
     elif opcao == '3':
-        print('\nExtrato\n')
-        if len(extrato) > 0:
-            print(extrato)
-            print(f'Saldo: R$ {saldo:.2f}')
-        else:
-            print('Não foram realizadas movimentações.')
+        exibir_extrato()
+
+    elif opcao == '4':
+        cadastrar_cliente()
+
+    elif opcao == '5':
+        criar_conta_corrente()
 
     elif opcao == '0':
         print('Sistema finalizado.')
         break
+
     else:
         print('Opção inválida!\nPor favor selecione uma das opções listadas.')
